22f66f97e1163c4babf2f05bd73cc74a2a882ead
Jason Hall <jasonbrianhall@gmail.com>
2022-12-30 18:11:38 -0500

    Bug fix

--- wizard.py
+++ wizard.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env
 import re
 
+def allocate_attributes(character):
+
+	regex=re.compile("[0-9]+")
+
+	print("Ok,", character.get("race"), "you have the following attributes :")
+	print("Strength = ", character.get("strength"), "Intelligence = ", character.get("intelligence"), "Dexterity = ", character.get("dexterity"))
+	print("and ", character.get("allocate"), "other points to allocate as you wish.")
+
+	for x in ["strength", "intelligence", "dexterity"]:
+		if character.get("allocate")>0:
+			print("\nHow many points do you wish to add to your " + x + "?")
+			data=""
+			while data=="":
+				try:
+					choice=input()[0]
+				except:
+					choice=""
+				if re.match(regex, choice) and int(choice)>=0 and int(choice)<=character.get("allocate"):
+					value=int(choice)
+					character[x]=character[x]+value
+					character["allocate"]=character["allocate"]-value
+					data="not empty"
+				else:
+					print("\n** How many points do you wish to add to your " + x + "?")
+			
+
+
 def select_race(character):
-    
+	
 
 	regex=re.compile("[edmh]")
 	datamapper={
-		"e": {"strength":  6, "intelligence": 8, "dexterity": 10, "name": "Elf"},
-		"d": {"strength": 10, "intelligence": 8, "dexterity":  6, "name": "Dwarf"},
-		"m": {"strength":  8, "intelligence": 8, "dexterity":  8, "name": "Man"},
-		"h": {"strength":  4, "intelligence": 8, "dexterity":  6, "name": "Hobbit"}
+		"e": {"strength":  6, "intelligence": 8, "dexterity": 10, "name": "Elf",	"allocate": 8},
+		"d": {"strength": 10, "intelligence": 8, "dexterity":  6, "name": "Dwarf",	"allocate": 8},
+		"m": {"strength":  8, "intelligence": 8, "dexterity":  8, "name": "Man",	"allocate": 8},
+		"h": {"strength":  4, "intelligence": 8, "dexterity": 12, "name": "Hobbit", "allocate": 4}
 		}
 
 	print("All right, bold one.")
@@ -25,21 +52,22 @@ def select_race(character):
 			choice=input()[0].lower()
 		except:
 			pass
-		if  re.match(regex, choice):
+		if	re.match(regex, choice):
 			race=choice
 		else:
 			print("** That was incorrect. Please type E, D, M, or H.")
 
-	character["race"]=datamapper.get(race).get("name")
-	character["strength"]=datamapper.get(race).get("strength")
-	character["dexterity"]=datamapper.get(race).get("dexterity")
-	character["dexterity"]=datamapper.get(race).get("intelligence")
+	character["race"]		  =datamapper.get(race).get("name")
+	character["strength"]	  =datamapper.get(race).get("strength")
+	character["dexterity"]	  =datamapper.get(race).get("dexterity")
+	character["intelligence"] =datamapper.get(race).get("intelligence")
+	character["allocate"]	  =datamapper.get(race).get("allocate")
 
 	return character
 
 
 def select_sex(character):
-    
+	
 	sex=""
 
 	datamapper={"m": "Male", "f": "Female"}
@@ -55,22 +83,22 @@ def select_sex(character):
 		if re.match(regex, choice):
 			sex=datamapper.get(choice)
 		else:
-			print("** Cute", character["race"], "real cute.  Try M or F.")
+			print("** Cute", character["race"], "real cute.	 Try M or F.")
 
 	character["sex"]=sex
-	return sex
+	return character
 
 def print_intro():
-	# Print Intro
+		# Print Intro
 
 	buffer=""
-	for x in range(0,64):
-		buffer=buffer+"*"
+	buffer=buffer+"*"*64
 
-	buffer=buffer+("\n\n                * * * THE WIZARD'S CASTLE * * *\n\n")
-	for x in range(0,64):
-		buffer=buffer+"*"
-	buffer=buffer+"\n\n"
+	buffer=buffer+("\n\n				* * * THE WIZARD'S CASTLE * * *\n\n")
+
+	buffer=buffer+"*"*64
+
+	buffer=buffer+"\n"*2
 	buffer=buffer+"""Many cycles ago, in the kindom of N'Dic, the gnomic
 wizard zot forged his great *Orb of Power*. He soon
 vanished, leaving behind his vast subterranean castle
@@ -89,7 +117,7 @@ def main():
 	print_intro()
 	character=select_race(character)
 	character=select_sex(character)	
-
+	character=allocate_attributes(character)
 
 main()
 
