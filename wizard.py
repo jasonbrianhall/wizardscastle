33a9ad70a2ea01014b1506c2142dcf9a4d41612d
Jason Hall <jasonbrianhall@gmail.com>
2022-12-30 18:16:51 -0500

    Latest

--- wizard.py
+++ wizard.py
@@ -3,103 +3,105 @@ import re
 
 def allocate_attributes(character):
 
-	regex=re.compile("[0-9]+")
+    regex=re.compile("[0-9]+")
 
-	print("Ok,", character.get("race"), "you have the following attributes :")
-	print("Strength = ", character.get("strength"), "Intelligence = ", character.get("intelligence"), "Dexterity = ", character.get("dexterity"))
-	print("and ", character.get("allocate"), "other points to allocate as you wish.")
+    print("Ok,", character.get("race"), "you have the following attributes :")
+    print("Strength = ", character.get("strength"), "Intelligence = ", character.get("intelligence"), "Dexterity = ", character.get("dexterity"))
+    print("and ", character.get("allocate"), "other points to allocate as you wish.")
 
-	for x in ["strength", "intelligence", "dexterity"]:
-		if character.get("allocate")>0:
-			print("\nHow many points do you wish to add to your " + x + "?")
-			data=""
-			while data=="":
-				try:
-					choice=input()[0]
-				except:
-					choice=""
-				if re.match(regex, choice) and int(choice)>=0 and int(choice)<=character.get("allocate"):
-					value=int(choice)
-					character[x]=character[x]+value
-					character["allocate"]=character["allocate"]-value
-					data="not empty"
-				else:
-					print("\n** How many points do you wish to add to your " + x + "?")
-			
+    for x in ["strength", "intelligence", "dexterity"]:
+        if character.get("allocate")>0:
+            print("\nHow many points do you wish to add to your " + x + "?")
+            data=""
+            while data=="":
+                try:
+                    choice=input()[0]
+                except:
+                    choice=""
+                if re.match(regex, choice) and int(choice)>=0 and int(choice)<=character.get("allocate"):
+                    value=int(choice)
+                    character[x]=character[x]+value
+                    character["allocate"]=character["allocate"]-value
+                    data="not empty"
+                else:
+                    print("\n** How many points do you wish to add to your " + x + "?")
+            
 
 
 def select_race(character):
-	
+    
 
-	regex=re.compile("[edmh]")
-	datamapper={
-		"e": {"strength":  6, "intelligence": 8, "dexterity": 10, "name": "Elf",	"allocate": 8},
-		"d": {"strength": 10, "intelligence": 8, "dexterity":  6, "name": "Dwarf",	"allocate": 8},
-		"m": {"strength":  8, "intelligence": 8, "dexterity":  8, "name": "Man",	"allocate": 8},
-		"h": {"strength":  4, "intelligence": 8, "dexterity": 12, "name": "Hobbit", "allocate": 4}
-		}
+    regex=re.compile("[edmh]")
+    datamapper={
+        "e": {"strength":  6, "intelligence": 8, "dexterity": 10, "name": "Elf",    "allocate": 8},
+        "d": {"strength": 10, "intelligence": 8, "dexterity":  6, "name": "Dwarf",  "allocate": 8},
+        "m": {"strength":  8, "intelligence": 8, "dexterity":  8, "name": "Man",    "allocate": 8},
+        "h": {"strength":  4, "intelligence": 8, "dexterity": 12, "name": "Hobbit", "allocate": 4}
+        }
 
-	print("All right, bold one.")
+    print("All right, bold one.")
 
-	choice=""
-	race=""
+    choice=""
+    race=""
 
-	while(race.lower()==""):
-		print("You may be an Elf, Dwarf, Man, or Hobbit")
+    while(race.lower()==""):
+        print("You may be an Elf, Dwarf, Man, or Hobbit")
 
-		try:
-			print("\nYour Choice? ")
-			choice=input()[0].lower()
-		except:
-			pass
-		if	re.match(regex, choice):
-			race=choice
-		else:
-			print("** That was incorrect. Please type E, D, M, or H.")
+        try:
+            print("\nYour Choice? ")
+            choice=input()[0].lower()
+        except:
+            pass
+        if  re.match(regex, choice):
+            race=choice
+        else:
+            print("** That was incorrect. Please type E, D, M, or H.")
 
-	character["race"]		  =datamapper.get(race).get("name")
-	character["strength"]	  =datamapper.get(race).get("strength")
-	character["dexterity"]	  =datamapper.get(race).get("dexterity")
-	character["intelligence"] =datamapper.get(race).get("intelligence")
-	character["allocate"]	  =datamapper.get(race).get("allocate")
+    character["race"]         =datamapper.get(race).get("name")
+    character["strength"]     =datamapper.get(race).get("strength")
+    character["dexterity"]    =datamapper.get(race).get("dexterity")
+    character["intelligence"] =datamapper.get(race).get("intelligence")
+    character["allocate"]     =datamapper.get(race).get("allocate")
 
-	return character
+    return character
 
 
 def select_sex(character):
-	
-	sex=""
+    
+    sex=""
 
-	datamapper={"m": "Male", "f": "Female"}
-	regex=re.compile("[mf]")
+    datamapper={"m": "Male", "f": "Female"}
+    regex=re.compile("[mf]")
 
-	choice=""
-	while(sex.lower()==""):
-		try:
-			print("\nWhich sex do you prefer? ")
-			choice=input()[0].lower()
-		except:
-			pass
-		if re.match(regex, choice):
-			sex=datamapper.get(choice)
-		else:
-			print("** Cute", character["race"], "real cute.	 Try M or F.")
+    choice=""
+    while(sex.lower()==""):
+        try:
+            print("\nWhich sex do you prefer? ")
+            choice=input()[0].lower()
+        except:
+            pass
+        if re.match(regex, choice):
+            sex=datamapper.get(choice)
+        else:
+            print("** Cute", character["race"], "real cute.  Try M or F.")
 
-	character["sex"]=sex
-	return character
+    character["sex"]=sex
+    return character
 
 def print_intro():
-		# Print Intro
+        # Print Intro
 
-	buffer=""
-	buffer=buffer+"*"*64
+    buffer=""
+    buffer=buffer+"*"*64
 
-	buffer=buffer+("\n\n				* * * THE WIZARD'S CASTLE * * *\n\n")
+    buffer=buffer+"\n\n"
+    buffer=buffer+" "*16
+    buffer=buffer+"* * * THE WIZARD'S CASTLE * * *\n\n"
 
-	buffer=buffer+"*"*64
+    buffer=buffer+"*"*64
 
-	buffer=buffer+"\n"*2
-	buffer=buffer+"""Many cycles ago, in the kindom of N'Dic, the gnomic
+    buffer=buffer+"\n"*2
+    buffer=buffer+"""Many cycles ago, in the kindom of N'Dic, the gnomic
 wizard zot forged his great *Orb of Power*. He soon
 vanished, leaving behind his vast subterranean castle
 filled with esurrient monsters, fabulous treasures, and
@@ -107,17 +109,18 @@ the incredible *Orb of Zot*. From that time hence, many
 a bold youth has ventured into the wizard's castle. as
 of now, *NONE* has ever emerged victoriously! Beware!!\n"""
 
-	print(buffer)
+    print(buffer)
 
 
 def main():
 
-	character={}	
-	
-	print_intro()
-	character=select_race(character)
-	character=select_sex(character)	
-	character=allocate_attributes(character)
+    character={}    
+    
+    print_intro()
+ 
+    character=select_race(character)
+    character=select_sex(character) 
+    character=allocate_attributes(character)
 
 main()
 
