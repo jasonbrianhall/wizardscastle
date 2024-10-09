25152f2f9b8c917cfefdec0d75640624a6f9c56f
Jason Hall <jasonbrianhall@gmail.com>
2024-10-09 12:27:31 -0400

    Latest

--- c/wizards-castle.c
+++ c/wizards-castle.c
@@ -936,7 +936,7 @@ char get_user_input()
         command = input[0];
 
         // Check if it's a valid command
-        if (strchr("NSEWUDMFLOGTHQ", command) != NULL) {
+        if (strchr("NSEWUDMFLOGTHQYN", command) != NULL) {
             return command;
         } else {
             print_message("Invalid command. Type 'H' for help.\n");
