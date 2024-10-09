82280e7198e38f4fa29182bff9c5ce4ac6b7bf60
Jason Hall <jasonbrianhall@gmail.com>
2024-10-09 18:33:50 -0400

    Latest

--- c/wizards-castle.h
+++ c/wizards-castle.h
@@ -4,6 +4,50 @@
 #define MAP_SIZE 512
 #define TREASURE_COUNT 8
 
+// Room type definitions
+#define EMPTY_ROOM 101
+#define ENTRANCE 102
+#define STAIRS_UP 103
+#define STAIRS_DOWN 104
+#define POOL 105
+#define CHEST 106
+#define GOLD 107
+#define FLARES 108
+#define WARP 109
+#define SINKHOLE 110
+#define CRYSTAL_ORB 111
+#define BOOK 112
+
+// Monster rooms (113-124)
+#define MONSTER_START 113
+#define KOBOLD 113
+#define ORC 114
+#define WOLF 115
+#define GOBLIN 116
+#define OGRE 117
+#define TROLL 118
+#define BEAR 119
+#define MINOTAUR 120
+#define GARGOYLE 121
+#define CHIMERA 122
+#define BALROG 123
+#define DRAGON 124
+#define MONSTER_END 124
+
+#define VENDOR 125
+
+// Treasures (126-133)
+#define TREASURE_START 126
+#define RUBY_RED 126
+#define NORN_STONE 127
+#define PALE_PEARL 128
+#define OPAL_EYE 129
+#define GREEN_GEM 130
+#define BLUE_FLAME 131
+#define PALANTIR 132
+#define SILMARIL 133
+#define TREASURE_END 133
+
 // Structures
 typedef struct {
     int race;
