36041051891c98c9a0f9d42bdbc4bf7d80d7be38
Jason Hall <jasonbrianhall@gmail.com>
2024-10-10 09:42:56 -0400

    Latest

--- c/main.c
+++ c/main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--debug") == 0) {
             debug_mode = true;
+            
             break;
         }
     }
