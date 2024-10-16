e53620ab2e260880bf985148a35f3bd997ded96f
Jason Hall <jasonbrianhall@gmail.com>
2024-10-16 13:40:17 -0400

    header files

---
+++ c/wizardioqt5.h
@@ -0,0 +1,26 @@
+#ifndef WIZARDIOQT5_H
+#define WIZARDIOQT5_H
+
+#include <QMainWindow>
+
+class WizardsCastleWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    WizardsCastleWindow(QWidget *parent = nullptr);
+    // Add other public methods as needed
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+WizardsCastleWindow* initialize_qt(int argc, char *argv[]);
+void close_qt(WizardsCastleWindow* window);
+
+// Declare other extern "C" functions here
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
