2b33232b77216f0b38bb3fe8d24a6e56ad71d7fd
Jason Hall <jasonbrianhall@gmail.com>
2024-10-20 21:48:54 -0400

    Stone armor

--- c/vendor.c
+++ c/vendor.c
@@ -53,7 +53,7 @@ void trade_with_vendor(Player *player, GameState *game)
                 print_message_formatted("Sold!\n");
             }
             else
-            {
+            { 
                if (random_number(8)==1 && player->weapon_type<4)
                {
                    print_message("I'll give you an extremely rare offer; sell %s for Excalibur? (Y/N) ", get_treasure_name(i), offer);
@@ -63,7 +63,17 @@ void trade_with_vendor(Player *player, GameState *game)
                        player->weapon_type=4;
                    }        
                }
-                 
+               else
+                   if (random_number(8)==1 && player->weapon_type<4)
+                   {
+                       print_message("I'll give you an extremely rare offer; sell %s for Stone Armor? (Y/N) ", get_treasure_name(i), offer);
+                       if (get_user_input_yn() == 'Y') {
+                           game->treasure[i] = 0;
+                           player->treasure_count--;
+                           player->armor_type=4;
+                           player->armor_points=42;
+                       }        
+                   }          
             }
         }
     }
