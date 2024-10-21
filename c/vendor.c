e38702ef6ee069f0d59d99f75e037505d4dc1a34
Jason Hall <jasonbrianhall@gmail.com>
2024-10-20 21:49:47 -0400

    oops weapon to armor

--- c/vendor.c
+++ c/vendor.c
@@ -64,7 +64,7 @@ void trade_with_vendor(Player *player, GameState *game)
                    }        
                }
                else
-                   if (random_number(8)==1 && player->weapon_type<4)
+                   if (random_number(8)==1 && player->armor_type<4)
                    {
                        print_message("I'll give you an extremely rare offer; sell %s for Stone Armor? (Y/N) ", get_treasure_name(i), offer);
                        if (get_user_input_yn() == 'Y') {
