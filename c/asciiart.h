aa2d28dab0396d4f899a7bedf0b48497dead87ed
Jason Hall <jasonbrianhall@gmail.com>
2024-10-28 16:53:14 -0400

    Latest

--- c/asciiart.h
+++ c/asciiart.h
@@ -9,22 +9,22 @@ const QString ENTRANCE_AA = "🏰";
 const QString STAIRSUP_AA = "⬆️";  
 const QString STAIRSDOWN_AA = "⬇️";
 const QString POOL_AA = "🌊";
-const QString CHEST_AA = "📦";
+const QString CHEST_AA = "🗝️🔓";
 const QString CRYSTALORB_AA = "🔮";
-const QString BOOK_AA = "📚";
+const QString BOOK_AA = "📚📖";
 const QString KOBOLD_AA = "👺";
 const QString ORC_AA = "👹";
 const QString WOLF_AA = "🐺";
-const QString GOBLIN_AA = "👿";
-const QString OGRE_AA = "👾";
-const QString TROLL_AA = "👻";
+const QString GOBLIN_AA = "👿🐒";
+const QString OGRE_AA = "👾🐉";
+const QString TROLL_AA = "👻🌋";
 const QString BEAR_AA = "🐻";
-const QString MINOTAUR_AA = "🐮";
-const QString GARGOYLE_AA = "🗿";
-const QString CHIMERA_AA = "🦁";
-const QString BALROG_AA = "👿";
+const QString MINOTAUR_AA = "🐃🧍⚔️";
+const QString GARGOYLE_AA = "👹🪨🦇";
+const QString CHIMERA_AA = "🦁🐐🐍";
+const QString BALROG_AA = "🔥👿";
 const QString DRAGON_AA = "🐉";
-const QString VENDOR_AA = "🏪";
+const QString VENDOR_AA = "🧙‍♂️🏪";
 
 }
 
